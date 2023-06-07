84216aa9654feabd0168b713805351587d11e258
Krossby <krosscaal@gmail.com>
2023-06-06 23:23:12 -0400

    feature: criando pacote cliente e criando entidade

---
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/cliente/ClienteEntity.java
@@ -0,0 +1,47 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ *
+ */
+
+package br.com.challenge.maidachallengedevelopingsnackbar.cliente;
+
+import br.com.challenge.maidachallengedevelopingsnackbar.model.User;
+import br.com.challenge.maidachallengedevelopingsnackbar.pedido.PedidoEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+//@Entity
+//@Table(name = "costumer")
+public class ClienteEntity extends User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @OneToMany(mappedBy = "cliente", fetch = FetchType.EAGER)
+  @Cascade(CascadeType.ALL)
+  private List<PedidoEntity> pedidos = new ArrayList<>();
+
+
+
+}
---
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/pedido/PedidoEntity.java
@@ -0,0 +1,79 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ *
+ */
+
+package br.com.challenge.maidachallengedevelopingsnackbar.pedido;
+
+import br.com.challenge.maidachallengedevelopingsnackbar.cliente.ClienteEntity;
+import br.com.challenge.maidachallengedevelopingsnackbar.domain.OrderStatus;
+import br.com.challenge.maidachallengedevelopingsnackbar.model.BaseEntity;
+import br.com.challenge.maidachallengedevelopingsnackbar.produto.ProdutoEntity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+//@Entity
+//@Table(name = "order")
+public class PedidoEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(
+      name = "costumer_id",
+      referencedColumnName = "id",
+      foreignKey = @ForeignKey(name = "fk_order_costumer_id"))
+  private ClienteEntity cliente;
+
+  @ManyToMany(
+      cascade = CascadeType.MERGE,
+      fetch = FetchType.LAZY)
+  @JoinTable(name = "orders_products",
+      joinColumns = {@JoinColumn(name = "order_id")},
+      inverseJoinColumns = {@JoinColumn(name = "product_id")})
+  private List<ProdutoEntity> produtos = new ArrayList<>();
+
+  @NotNull
+  @Column(name = "order_date")
+  private LocalDateTime dataPedido;
+
+  @NotNull
+  @Column(name = "total_price")
+  private BigDecimal valor;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private OrderStatus status;
+}
