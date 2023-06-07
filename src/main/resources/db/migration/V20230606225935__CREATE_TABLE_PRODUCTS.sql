6dcb8e638c759e58d81735dab6a86a7a0219b152
Krossby <krosscaal@gmail.com>
2023-06-06 23:24:25 -0400

    feature: criando migrations manager, products, snack_bar

---
+++ src/main/resources/db/migration/V20230606225935__CREATE_TABLE_PRODUCTS.sql
@@ -0,0 +1,10 @@
+create table if not exists products (
+      id  bigserial not null,
+      created_at TIMESTAMP,
+      updated_at TIMESTAMP,
+      description varchar(255) not null,
+      name varchar(100) not null,
+      product_price numeric(19, 2) not null,
+      product_type varchar(255) not null,
+      primary key (id)
+);
