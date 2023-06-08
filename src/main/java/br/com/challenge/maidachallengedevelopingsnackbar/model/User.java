ae81690f8f1eb94a966581479ae36a48fe087ea9
Krossby <krosscaal@gmail.com>
2023-06-08 16:15:06 -0400

    refactor: refatorando classe User

--- src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/model/User.java
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/model/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import lombok.Getter;
@@ -21,27 +22,17 @@ import org.hibernate.validator.constraints.Length;
 public abstract class User extends BaseEntity{
 
   @Column(name = "name")
-  @NotBlank
-  @Size(max = 100)
   private String nome;
 
-  @Email
-  @NotBlank
-  @Size(max = 100)
   private String email;
 
   @Column(name = "password")
-  @NotBlank
-  @Length(min = 6, max = 100)
   private String senha;
 
   @Column(name = "fone_number")
-  @NotBlank
-  @Size(max = 15)
   private Integer telefone;
 
-  @NotBlank
   @Column(name = "birth_date")
-  private Date datanascimento;
+  private Date dataNascimento;
 
 }
