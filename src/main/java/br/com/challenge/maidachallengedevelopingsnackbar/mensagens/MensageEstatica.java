4a417e81ccb8ba59f534378b4c0b0fa448177a60
Krossby <krosscaal@gmail.com>
2023-06-09 18:33:36 -0400

    feat: criando mensagens estaticas de erro

---
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/mensagens/MensageEstatica.java
@@ -0,0 +1,23 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ *
+ */
+
+package br.com.challenge.maidachallengedevelopingsnackbar.mensagens;
+
+public class MensageEstatica {
+  public final static String PRODUCT_EXISTS = "Já existe um produto cadastrado com esse nome!";
+  public final static String PRODUCT_NOT_FOUND = "Produto não encontrado!";
+  public final static String PRODUCT_NAME_ERROR = "Campo nome somente pode conter letras y números sem caracteres especiais, minimo 5 caracteres sem contar espaços em branco";
+  public final static String PRODUCT_QUANTITY_ERROR = "Campo quantidade deve ser maior o igual ao campo quantidadeMinima";
+  public final static String PRODUCT_PRICE_ERROR = "Campo preço deve ser maior a zero";
+  public final static String MANAGER_EXISTS ="Gestor já foi cadastrado, somente pode cadastrar um único Gestor!";
+  public final static String MANAGER_NOT_FOUND = "Gestor não encontrado!";
+  public final static String MANAGER_NAME_ERROR = "Campo nome deve conter somente letras sem carateres especiais, mínimo 5 caracteres sem contar espaços em branco!";
+  public final static String MANAGER_TELEFONE_ERROR = "Campo telefone deve conter somente números sem caracteres especiais!";
+  public final static String COSTUMER_EMAIL_EXISTS ="Já existe um cliente cadastrado com e-mail informado!";
+  public final static String COSTUMER_NOT_FOUND = "Cliente não foi encontrado!";
+  public final static String COSTUMER_NAME_ERROR = "Campo nome deve conter somente letras sem carateres especiais, mínimo 4 caracteres sem contar espaços em branco!";
+  public final static String COSTUMER_TELEFONE_ERROR = "Campo telefone deve conter somente números sem caracteres especiais!";
+
+}
