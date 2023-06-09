b96b78ce8f1d73cb7a052c58a12f8b445c47da63
Krossby <krosscaal@gmail.com>
2023-06-09 18:37:39 -0400

    feat: time para UTC

--- src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/MaidaChallengeDevelopingSnackBarApplication.java
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/MaidaChallengeDevelopingSnackBarApplication.java
@@ -1,5 +1,6 @@
 package br.com.challenge.maidachallengedevelopingsnackbar;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class MaidaChallengeDevelopingSnackBarApplication {
 
   public static void main(String[] args) {
 
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     SpringApplication.run(MaidaChallengeDevelopingSnackBarApplication.class, args);
   }
 
