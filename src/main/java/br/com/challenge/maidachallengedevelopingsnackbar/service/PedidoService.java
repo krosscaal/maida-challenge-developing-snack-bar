9c206ecb8e01bc277befe4a7757d513a9e1961c3
Krossby <krosscaal@gmail.com>
2023-06-12 13:30:01 -0400

    fix: removendo codigo em PedidoService

--- src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/service/PedidoService.java
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/service/PedidoService.java
@@ -66,39 +66,6 @@ public class PedidoService {
     return pedidoDtoStatus;
   }
 
-//  public PedidoDtoStatus updatePedido(final Long pedido_id, final PedidoDto dto) {
-//    this.gestorService.findGestor();
-//
-//    final Optional<PedidoEntity> optionalPedidoEntityPersisted = this.findPedido(pedido_id);
-//    final ClienteEntity clienteEntityObj = this.clienteService.findCliente(dto.getCliente_id()).get();
-//
-//    if (!optionalPedidoEntityPersisted.get().getStatus().equals(DomainOrderStatus.REQUESTED)) {
-//      throw new BusinessException(ORDER_CANNOT_BE_MODIFIED);
-//    }
-//    if (!optionalPedidoEntityPersisted.get().getCliente().equals(clienteEntityObj)) {
-//      throw new BusinessException(ORDER_DOES_NOT_BELONG_TO_THE_CUSTOMER);
-//    }
-//    final PedidoEntity pedidoEntityUpdate = this.validarPedidoDtoparaUpdate(optionalPedidoEntityPersisted.get(), dto);
-//
-//    pedidoEntityUpdate.setId(optionalPedidoEntityPersisted.get().getId());
-//    pedidoEntityUpdate.setDataPedido(optionalPedidoEntityPersisted.get().getDataPedido());
-//    pedidoEntityUpdate.setCreatedAt(optionalPedidoEntityPersisted.get().getCreatedAt());
-//    pedidoEntityUpdate.setStatus(optionalPedidoEntityPersisted.get().getStatus());
-//    this.repository.save(pedidoEntityUpdate);
-//
-//    final List<ItemPedidoDto> listItemPedidoDto =
-//        this.produtoService.listProdutosEmPedido(pedidoEntityUpdate.getItens());
-//
-//    PedidoDtoStatus pedidoDtoStatus =
-//        new PedidoDtoStatus(
-//            pedidoEntityUpdate.getId(),
-//            listItemPedidoDto,
-//            pedidoEntityUpdate.getValor(),
-//            pedidoEntityUpdate.getStatus());
-//
-//    return pedidoDtoStatus;
-//  }
-
   public PedidoDtoStatus getPedido(final Long id) {
     this.gestorService.findGestor();
     final PedidoEntity pedidoEntityPersisted = this.findPedido(id).get();
@@ -188,71 +155,6 @@ public class PedidoService {
     pedidoEntity.setStatus(DomainOrderStatus.REQUESTED);
     return pedidoEntity;
   }
-  private PedidoEntity validarPedidoDtoparaUpdate(final PedidoEntity pedidoEntity, final PedidoDto dto) {
-    ClienteEntity clienteEntityPersisted =
-        this.clienteService.findCliente(dto.getCliente_id()).get();
-
-    final List<ItemPedidoEntity> itens = pedidoEntity.getItens();
-
-    List<PedidoDtoProdutoQuantidade> listaProdutoQuantidadeNovos = new ArrayList<>();
-    List<ItemPedidoEntity> itensParaRemover = new ArrayList<>();
-    /* cria lista itens para remover*/
-    itens.forEach(itemPedidoEntity -> {
-      final Long itemPedidoEntity_Id = itemPedidoEntity.getId();
-      final Long produto_id = itemPedidoEntity.getProduto().getId();
-      boolean notfound = false;
-      for (PedidoDtoProdutoQuantidade pedidoDtoProdutoQuantidade : dto.getProdutos()) {
-        if(!pedidoDtoProdutoQuantidade.getProduto_id().equals(produto_id)) {
-          notfound = true;
-        }
-      }
-      if(notfound) {
-        itensParaRemover.add(itemPedidoEntity);
-      }
-    });
-
-    /* atualizando quantidade se o item existe*/
-    dto.getProdutos()
-          .forEach(pedidoDtoProdutoQuantidade -> {
-            final Long produtoId = pedidoDtoProdutoQuantidade.getProduto_id();
-            final Integer quantidade = pedidoDtoProdutoQuantidade.getQuantidade();
-            final ProdutoEntity produtoEntity = this.produtoService.findProduto(produtoId).get();
-            boolean produto_idExists = false;
-            for (ItemPedidoEntity item: itens) {
-              if(item.getProduto().getId().equals(produtoId)) {
-                item.setQuantidade(quantidade);
-                produto_idExists = true;
-                break;
-              }
-            }
-            if (!produto_idExists) {
-              listaProdutoQuantidadeNovos.add(pedidoDtoProdutoQuantidade);
-            }
-          });
-    /* adicionando novos itens */
-    listaProdutoQuantidadeNovos
-        .forEach(pedidoDtoProdutoQuantidade -> {
-          final Long produtoId = pedidoDtoProdutoQuantidade.getProduto_id();
-          final Integer quantidade = pedidoDtoProdutoQuantidade.getQuantidade();
-          final ProdutoEntity produtoEntity = this.produtoService.findProduto(produtoId).get();
-          pedidoEntity.adicionarItem(new ItemPedidoEntity(quantidade, pedidoEntity, produtoEntity));
-        });
-
-
-    /* valor total*/
-    final List<ItemPedidoEntity> itensFinais = pedidoEntity.getItens();
-
-    Double novoValorTotal = 0.0;
-    for (ItemPedidoEntity itemPedido : itensFinais){
-      final double valor = itemPedido.getQuantidade() * itemPedido.getPrecoUnitario().doubleValue();
-      novoValorTotal+=valor;
-    }
-    pedidoEntity.setValor(new BigDecimal(novoValorTotal));
-
-    return pedidoEntity;
-  }
-
-
   private Optional<PedidoEntity> findPedido(final Long id) {
     final Optional<PedidoEntity> optionalObj = this.repository.findById(id);
     if(optionalObj.isEmpty()) {
