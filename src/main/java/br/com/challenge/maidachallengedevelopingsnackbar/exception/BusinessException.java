d75c8fa923ae22d5c926e01119d4a00f9ea879ba
Krossby <krosscaal@gmail.com>
2023-06-12 13:05:42 -0400

    fix: atualizando BusinessException

--- src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/exception/BusinessException.java
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/exception/BusinessException.java
@@ -10,6 +10,7 @@ public class BusinessException extends RuntimeException{
   private static final long serialVersionUID = 1L;
 
   public BusinessException(String mensagem) {
+
     super(mensagem);
   }
 
