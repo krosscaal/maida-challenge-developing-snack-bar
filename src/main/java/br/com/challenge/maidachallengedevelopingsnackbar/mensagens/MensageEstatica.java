01805de7231531aba7b3bb6ba0517afbd3b868b0
Krossby <krosscaal@gmail.com>
2023-06-12 13:09:42 -0400

    feat: add nova MensageEstatica error na quantidade

--- src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/mensagens/MensageEstatica.java
+++ src/main/java/br/com/challenge/maidachallengedevelopingsnackbar/mensagens/MensageEstatica.java
@@ -12,7 +12,7 @@ public class MensageEstatica {
   public final static String PRODUCT_QUANTITY_ERROR = "Campo quantidade deve ser maior o igual ao campo quantidadeMinima";
   public final static String PRODUCT_PRICE_ERROR = "Campo preço deve ser maior a zero";
   public final static String MANAGER_EXISTS ="Gestor já foi cadastrado, somente pode cadastrar um único Gestor!";
-  public final static String MANAGER_NOT_FOUND = "Gestor não encontrado!";
+  public final static String MANAGER_NOT_FOUND = "Gestor não encontrado, necesario cadastrar um Gestor para continuar!";
   public final static String MANAGER_NAME_ERROR = "Campo nome deve conter somente letras sem carateres especiais, mínimo 5 caracteres sem contar espaços em branco!";
   public final static String TELEFONE_ERROR = "Campo telefone deve conter somente números!";
   public final static String COSTUMER_EMAIL_EXISTS ="Já existe um cliente cadastrado com e-mail informado!";
@@ -23,4 +23,5 @@ public class MensageEstatica {
   public final static String ORDER_IS_NOT_IN_REQUESTED_STATUS = "Pedido não encontra-se com status SOLICITADO e não pode ser cancelado";
   public final static String ORDER_CANNOT_BE_MODIFIED = "Pedido não encontra-se com status SOLICITADO e não pode ser modifcado!";
   public final static String ORDER_DOES_NOT_BELONG_TO_THE_CUSTOMER = "Este pedido não pertence ao cliente informado!";
+  public final static String QUANTITY_ERROR = "Quantidade deve ser maior a zero ";
 }
